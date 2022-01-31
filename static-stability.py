a1dff5fa6478c7e9e8f6561c7131641aa89c5f3e
sysmex <abrahamgmacias@hotmail.com>
2022-01-31 16:20:22 -0700

    Updated solver

--- static-stability.py
+++ static-stability.py
@@ -67,3 +67,15 @@ sae.addCoefficient(epsilon_a, 'epsilon_a')
 
 CL_0 = sae_longitudinal.liftCoefZero()
 sae.addCoefficient(CL_0, 'CL_0')
+
+alpha_0 = sae_longitudinal.alphaZero()
+sae.addSpec(alpha_0, 'alpha_0')
+cm_0 = sae_longitudinal.cmZero(alpha_0)
+sae.addCoefficient(cm_0, 'Cm_0')
+
+# Compute for Cm_alpha
+cm_a = sae_longitudinal.cmAlpha() 
+sae.addCoefficient(cm_a, 'Cm_a')
+
+# Compute Equilibrium Angle - deg
+a_e = sae_longitudinal.alphaEquilibrium()*57.3
