736bf6bd1a96008dd379e5584e64242251b0333f
sysmex <abrahamgmacias@hotmail.com>
2022-02-05 12:12:26 -0700

    Updated trim_analysis solver

--- stability/trim_analysis.py
+++ stability/trim_analysis.py
@@ -1,20 +1,21 @@
-from trim import Aerodynamics as aero
+from resources.aircraft_data import sae
+from resources.aircraft_functions import Aerodynamics as aero
 
 rangoDeVelocidades = []
 
-# Se extraen los valores del CL y CD del aeronave completa via X5
-# 
+# Extract data from sae class...
+print(sae.getComponents('motor'))
 
 for v in rangoDeVelocidades:
-    cLift = aero.liftCoefficient(wto, rho, v, Sw)
-    cDrag = aero.dragCoefficient(CD0, CL, CL_min_D, ew, ARw)
+    coefLift = aero.liftCoefficient(wto, rho, v, Sw)
+    coefDrag = aero.dragCoefficient(CD0, coefLift, CL_min_D, ew, ARw)
 
     # Def these functions below...
     alpha = aero.Closest(CL_list, Angles_list, CL)
 
-    aeroEfficiency = 
-    tRequired = aero.TReq(wto, CD, CL)
-    pRequired = aero.PReq(TR, v)
+    aeroEfficiency = coefLift / coefDrag
+    thrustReq = aero.thrustRequired(wto, coefDrag, coefLift)
+    powerReq = aero.powerRequired(thrustReq, v)
 
     pAvailable = PA_max*n_ef*(rho / rho_list[0])
     rClimb = af.R_C(PA, PR, wto)
@@ -22,4 +23,3 @@ for v in rangoDeVelocidades:
     # Appending section
 
 # Plotting section
-
