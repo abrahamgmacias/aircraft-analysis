a708d23d690f03254efb4a9b6931cbbe979408af
sysmex <abrahamgmacias@hotmail.com>
2022-02-05 12:11:24 -0700

    Updated class instantiation

--- resources/aircraft_data.py
+++ resources/aircraft_data.py
@@ -9,16 +9,18 @@ uas = {'mass': 22, 'ARw': 7.5, 'Sw': 1.831, 'cw': 0.494, 'iw': round(1/57.3, 4),
 # Longitudinal Coefficients / general_coefficients 
 long_coeffs = {'aw': 4.40637, 'at': 3.63855, 'ew': 0.825, 'Cm_0w': -0.208, 'CL_0w': 0.62, 'CL_0': 0.6096, 'CD_0': 0.03324}
 
-# Lateral Coefficients / general_coefficients
-lat_coeffs = {}
-
 
 # --------- Extra Computation ---------- # 
 # Class Instantiation
 sae = af.Aircraft()
 sae.addWeights(mtow=22)
-sae.addCoefficients(cl_list=[], cd_list=[], cd_0=0.03324, cl_min_d=0, cl_0=0.6096)
+sae.addCoefficients(cl_list=[], cd_list=[])      # Pull from excel file...
+sae.addCoefficients(cd_0=0.03324, cl_min_d=0, cl_0=0.6096)
 
-wing = af.Wing('saeWing', 3.71, 0.494, 'MH-114')
+wing = af.Wing(3.71, 0.494, 'MH-114')
 wing.addGeometry(ARw=7.5, Sw=1.831)
-wing.addCoefficients(ew=0.825, )
+wing.addCoefficients(ew=0.825)
+
+propeller = af.Propeller(12, 12)   # Change data...
+motor = af.Motor(propeller)
+sae.addComponents(motor=motor)
